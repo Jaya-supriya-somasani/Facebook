807a437d07b72bd3cc2a97f3b1c28e03806ff39b
jayasupriyasomasani <somasanijayasupriya@gmail.com>
2022-07-05 15:35:10 +0530

    initial commit

---
+++ app/src/main/java/com/example/facebook/CreateAccountNamePageFragment.kt
@@ -0,0 +1,19 @@
+package com.example.facebook
+
+import com.example.facebook.util.BaseFragment
+import com.example.facebook.viewmodels.CreateAccountViewModel
+
+class CreateAccountNamePageFragment:
+    BaseFragment<com.example.facebook.databinding.FragmentCreateAccountNamePagBinding, CreateAccountViewModel>() {
+    override fun getViewModel(): Class<CreateAccountViewModel> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getResourceId(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun initViews() {
+        TODO("Not yet implemented")
+    }
+}
